262a8addae839bdd99353a137d38808dea91eed0
Joseph Amash <163041660+JosephAmash@users.noreply.github.com>
2025-06-03 22:03:53 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,17 +1,5 @@
-// add class active to header on scroll
+var Nav_Links = document.querySelector(".nav_links");
 
-let header = document.querySelector("header")
-
-window.onscroll = function () {
-    if (this.scrollY >= 50) {
-        header.classList.add("active")
-    } else {
-        header.classList.remove("active")
-    }
-}
-
-let nav_links = document.getElementById("links");
-
-function Open_colose_Menu{
-    nav_links.classList.toggle("active")
+function Open_CloseMenu() {
+    Nav_Links.classList.toggle("active");
 }
